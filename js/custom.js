cb560cb9000b73b008213756fa8430632c2076eb
Pascal-Osemene <pascalosemene@gmail.com>
2023-02-09 05:59:27 +0100

    made some corrections

--- js/custom.js
+++ js/custom.js
@@ -26,3 +26,22 @@
   });
 
 })(jQuery); // End of use strict
+
+//  Contact Mailing script
+
+function sendEmail(){
+  Email.send({
+    // SecureToken : " a7ef66fd-849d-4a6f-8c9a-36ebf571579c ",
+      Host : "smtp.gmail.com",
+      Username : "pascalosemene@gmail.com",
+      Password : "2D1861F199C1F67ACF9A7C5138FBC2AEB0AE",
+      To : 'pascalosemene@gmail.com',
+      From : document.getElementById("email").value,
+      Subject : document.getElementById("subject").value,
+      Body : "Name: " + document.getElementById("name").value
+              + "<br> Email: " + document.getElementById("email").value
+              + "<br> Message: " + document.getElementById("message").value
+  }).then(
+    message => alert("Message sent successfully")
+  );
+}
